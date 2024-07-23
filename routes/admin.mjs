85b709b037a4e6f7084e493da7b494b47c8bd837
Khanh Le <kle01@villanova.edu>
2024-07-23 09:49:56 +0700

    admin end membership

--- routes/admin.mjs
+++ routes/admin.mjs
@@ -13,7 +13,7 @@ import mongoose from "mongoose"
 const router = Router()
 
 //register new membership
-router.post("/api/admin/register/:_id", adminAuth, async (request, response) => {
+router.patch("/api/admin/membership/:_id", adminAuth, async (request, response) => {
     try {
         const {_id} = request.params
         if (!mongoose.Types.ObjectId.isValid(_id)) {
@@ -25,59 +25,73 @@ router.post("/api/admin/register/:_id", adminAuth, async (request, response) => 
             return response.status(400).send({ message: "User not found" })
         }
         //get the duration
-        let {duration, startDate} = request.body //get the duration from the body
+        let {duration} = request.body //get the duration from the body
         if (!duration || duration < 0){
             duration = 2 //if null set to default 2 months
         }
+
         duration = Math.round(duration) //make sure duration is integer value
 
-        if (!startDate){
-            
-            startDate = new Date() //if null set to current date
-        } else if (startDate < new Date()){
-            return response.status(400).send({ message: "Start date cannot be in the past" })
-        } else {
-            startDate = new Date(startDate) //convert string to date
-        }
-        //check if exisiting membership
-        let recentStartDate
-        let recentEndDate
-        let membership = {}
-        //console.log(queriedUser["membership"].length)
-        if (queriedUser["membership"].length > 0){ //if exist -> have to check
-        recentStartDate = queriedUser["membership"][queriedUser["membership"].length - 1].startDate
-        recentEndDate = queriedUser["membership"][queriedUser["membership"].length - 1].endDate
-    
-       
-        if (recentEndDate >= startDate){ 
-            console.log(1)
-            //extend membership duration
-            let newEndDate = new Date(recentEndDate);
-            newEndDate.setMonth(newEndDate.getMonth() + duration);
-            membership.startDate = recentEndDate
-            membership.endDate = newEndDate
-        
-        } 
-        } else {
-           
-            let newEndDate = new Date();
-            newEndDate.setMonth(newEndDate.getMonth() + duration);
-            membership.startDate = startDate
-            membership.endDate = newEndDate
-        }
-        console.log(membership)
-        
-        queriedUser["membership"].push(membership)
-        
-        
-        const updatedMembership = await queriedUser.save()
-        return response.status(200).send(updatedMembership)
+        const membership = {
+            startDate: new Date(),
+            endDate: new Date()
+        };
+        membership.endDate.setMonth(membership.endDate.getMonth() + duration);
 
-    } catch (error) {
+        if (queriedUser.membership.length > 0) { //check for existing memberships
+            const lastMembership = queriedUser.membership[queriedUser.membership.length - 1];
+            if (lastMembership.endDate >= new Date()) {
+                /* membership.startDate = lastMembership.endDate;
+                membership.endDate = new Date(lastMembership.endDate);
+                membership.endDate.setMonth(membership.endDate.getMonth() + duration); */
+                let extendEndDate = new Date(lastMembership.endDate)
+                extendEndDate.setMonth(extendEndDate.getMonth() + duration)
+                lastMembership.endDate = extendEndDate
+                const updatedUser = await queriedUser.save();
+                return response.status(200).send(updatedUser);
+            }
+            }
+            queriedUser.membership.push(membership);
+            const updatedUser = await queriedUser.save();
+            return response.status(200).send(updatedUser);
+
+
+        } catch (error) {
         return response.status(400).send(error.message)
     }
     
 
+})
+
+router.delete("/api/admin/membership/:_id", adminAuth, async (request, response) => {
+    try {
+        const {_id} = request.params
+        if (!mongoose.Types.ObjectId.isValid(_id)) {
+            return response.status(400).send({ message: 'Invalid ID format' });
+            }
+
+        const queriedUser = await User.findById(_id)
+
+        if (!queriedUser) {
+            return response.status(400).send({ message: "User not found" })
+        }
+
+        if (queriedUser.membership.length > 0){
+            const lastMembership = queriedUser.membership[queriedUser.membership.length - 1];
+            lastMembership.endDate = new Date()
+            const updatedUser = await queriedUser.save();
+            return response.status(200).send(updatedUser);
+        } else {
+            return response.status(400).send({ message: "User has no active membership" })
+        }
+
+
+        } catch (error) {
+            console.log(error.message)
+            return response.status(400).send(error)
+        }  
+            
+    
 })
 
 
